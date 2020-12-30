b0c90c6a53707ea81c1f68b2c6a07e0825b6045b
faded19 <306165135@qq.com>
2020-12-30 17:09:52 +0800

    更新

--- home/assets/js/index.js
+++ home/assets/js/index.js
@@ -1,84 +1,58 @@
 $(function() {
-    //调用getUserinfo  获取用户基本信息
-    grtUserInfo();
+    getUserInfo();
 
-    $('#btnLogin').on('click', function() {
-        layer.confirm(
-            '确认退出', { icon: 3, title: '提示' },
-
-            function(index) {
-                // do aomething
-                // 1. 清空本地储存中的token
-                localStorage.removeItem('token');
-
-                // 2.重新跳转到登录页面
-                location.href = '/login.html'
-
-                // 关闭confirm询问框
-                layer.close(index)
+    $("#btnLogout").on("click", function() {
+        // 提示用户是否要删除
+        layer.confirm("确认退出?", { icon: 3, title: "提示" }, function(index) {
+            // 清除本地存储token
+            localStorage.removeItem("token");
+            // 重新跳转到登录页面
+            location.href = "login.html";
+            // 关闭confirm询问框
+            layer.close(index);
+        });
+    });
+});
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: "GET",
+        url: "/my/userinfo",
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg("获取用户信息失败！");
             }
-
-        );
-    })
-
-
-    function grtUserInfo() {
-        $.ajax({
-            method: "GET",
-            url: "/my/userinfo",
-
-            // headers  请求头配置对象
-            // headers: {
-            //     Authorization: localStorage.getItem("token") || ''
-            // },
-            success: function(res) {
-                console.log(res);
-
-                if (res.status !== 0) {
-                    layui.layer.mag(res.message);
-                    return;
-                }
-
-                // 调用renderAvatar   渲染头像
-                renderAvatar(res.data);
-            },
-
-            error(err) {
-                complete: function(res) {
-                    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败')
-
-                }
-            }
-        })
+            // 调用 renderAvatar 渲染用户的头像
+            renderAvatar(res.data);
+        },
+        error(err) {},
+        /*  complete: function (res) {
+          console.log(res);
+          if (
+            res.responseJSON.status === 1 &&
+            res.responseJSON.message === "身份认证失败！"
+          ) {
+            localStorage.removeItem("token");
+            location.href = "/login.html";
+          }
+        }, */
+    });
+}
+// 渲染用户的头像
+function renderAvatar(user) {
+    // 1. 获取用户的名称
+    var name = user.nickname || user.username;
+    // 2. 设置欢迎的文本
+    $("#welcome").html("欢迎&nbsp;&nbsp;" + name);
+    // 3. 按需渲染用户的头像
+    if (user.user_pic !== null) {
+        // 3.1 渲染图片头像
+        $(".layui-nav-img").attr("src", user.user_pic).show();
+        $(".text-avatar").hide();
+    } else {
+        // 3.2 渲染文本头像
+        $(".layui-nav-img").hide();
+        var first = name[0].toUpperCase();
+        $(".text-avatar").html(first).show();
     }
-
-
-    // 渲染用户的头像
-    function renderAvatar(user) {
-
-        //1.获取用户的名称
-        const name = user.nickname || user.sername;
-        console.log(name);
-
-        $('#welcome').html('欢迎${name}');
-
-        if (user.user_pic) {
-            // 图片头像
-            $('.layui-nav-img').attr('src', user.user_pic).show();
-            $('.text-avatar').hide();
-        } else {
-
-            // 文字头像
-            const first = name[0].toUpperCase();
-            $('.text-avatar').html(first).show();
-            $('.layui-nav-img').hide();
-        }
-
-
-
-
-
-    }
-})
-
-//获取用户的基本信息
+}
--- home/assets/js/login.js
+++ home/assets/js/login.js
@@ -1,74 +1,61 @@
 $(function() {
-    // 点击“去注册账号”的链接
-    $('#link_reg').on('click', function() {
-        $('.login-box').hide()
-        $('.reg-box').show()
-    })
-
-    // 点击“去登录”的链接
-    $('#link_login').on('click', function() {
-        $('.login-box').show()
-        $('.reg-box').hide()
-    })
-
-
-
-    var form = layui.form
-    var layer = layui.layer
-        // 从layui中获取form对象
+    $("#link_reg").on("click", function() {
+        $(".login-box").hide();
+        $(".reg-box").show();
+    });
+    // 点击登录按钮
+    $("#link_login").on("click", function() {
+        $(".reg-box").hide();
+        $(".login-box").show();
+    });
+    // 从layui获取form表单
+    const { form, layer } = layui;
     form.verify({
-
-        // 自定义了一个叫做 pwd 校验规则
-        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-
-        // 校验两次密码是否一致的规则
+        pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
         repwd: function(value) {
-            // 通过形参拿到的是确认密码框中的内容
-            // 还需要拿到密码框中的内容
-            // 然后进行一次等于的判断
-            // 如果判断失败,则return一个提示消息即可
-            var pwd = $('.reg-box [name=password]').val()
+            const pwd = $(".reg-box [name=password]").val();
             if (pwd !== value) {
-                return '两次密码不一致！'
+                return "俩次密码不一致";
             }
-        }
-    })
-
-    $('#form_reg').on('submit', function(e) {
-        e.preventDefault()
+        },
+    });
+    // 监听表单提交事件
+    $("#form_reg").on("submit", function(e) {
+        e.preventDefault();
         $.ajax({
-            url: '/api/reguser',
-            method: 'POST',
+            url: "/api/reguser",
+            method: "POST",
             data: {
-                username: $('#form_reg [name=username]').val(),
-                password: $('#form_reg [name=password]').val()
+                username: $("#form_reg [name=username]").val(),
+                password: $("#form_reg [name=password]").val(),
             },
             success: (res) => {
                 if (res.status !== 0) {
-                    layer.msg(res.message || '注册失败')
+                    layer.msg(res.message || "注册失败");
+                    return;
                 }
-                layer.msg(res.message || '注册成功');
+                layer.msg(res.message || "注册成功");
+                $("#link_login").click();
+            },
+        });
+    });
 
-                //自动跳转
-                $('#link_login').click()
-            }
-        })
-    })
-    $('#form_login').on('submit', function(e) {
+    // 监听登录表单事件
+    $("#form_login").on("submit", function(e) {
         e.preventDefault();
         $.ajax({
-            url: '/api/login',
-            method: 'POST',
+            url: "/api/login",
+            method: "POST",
             data: $(this).serialize(),
             success: function(res) {
                 if (res.status !== 0) {
                     layer.msg(res.message);
                     return;
                 }
-                layer.msg('登陆成功')
-                localStorage.getItem('token', res.token)
-                location.href = '/index.html'
-            }
-        })
-    })
-})
+                layer.msg("登录成功");
+                localStorage.setItem("token", res.token);
+                location.href = "/index.html";
+            },
+        });
+    });
+});
